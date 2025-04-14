05d8835131f9531a6f1778d343a05cec43c40d06
agantika-wadikar <wadikaraganika296@gmail.com>
2025-04-14 10:10:24 +0530

    changes made

--- src/main/java/com/codit/IplRestapiApplication.java
+++ src/main/java/com/codit/IplRestapiApplication.java
@@ -8,6 +8,7 @@ public class IplRestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IplRestapiApplication.class, args);
+		System.out.println("Line is added");
 	}
 
 }
